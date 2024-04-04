ac2ba7622a589209da2c0e2e7110a9416852b5fb
MartotoBG <martingav123@gmail.com>
2024-04-04 17:53:39 +0300

    finish the hangman

--- hangman/src/App.tsx
+++ hangman/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 import words from "./wordList.json";
 import { HangmanDrawing } from "./HangmanDrawing";
 import { HangmanWord } from "./HangmanWord";
@@ -8,6 +8,41 @@ function App() {
     return words[Math.floor(Math.random() * words.length)];
   })
   const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+  const incorrectLetters = guessedLetters.filter(letter => !wordToGuess.includes(letter));
+
+  const addGuessedLetter = useCallback((letter: string) => {
+    if (guessedLetters.includes(letter) || isLoser || isWinner) return
+    setGuessedLetters(currentLetters => [...currentLetters, letter]);
+  }, [guessedLetters])
+  
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key;
+      if (!key.match(/^[a-z]$/)) return
+      e.preventDefault();
+      addGuessedLetter(key);
+    }
+    document.addEventListener("keypress", handler);
+    return () => {
+      document.removeEventListener("keypress", handler);
+    }
+  }, [guessedLetters])
+  
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key;
+      if (key !== "Enter") return
+      e.preventDefault();
+      setGuessedLetters([]);
+      setWordToGuess(words[Math.floor(Math.random() * words.length)]);
+    }
+    document.addEventListener("keypress", handler);
+    return () => {
+      document.removeEventListener("keypress", handler);
+    }
+  }, [])
+  const isLoser = incorrectLetters.length >= 6;
+  const isWinner = wordToGuess.split("").every(letter => guessedLetters.includes(letter));
   return (
     <div style={{
       maxWidth: "800px",
@@ -17,10 +52,21 @@ function App() {
       margin: "0 auto",
       alignItems: "center"
     }}>
-      <div style={{ fontSize: "2rem", textAlign: "center" }}>Lose Win</div>
-      <HangmanDrawing />
-      <HangmanWord />
-      <Keyboard/>
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>
+        {isWinner && "Winner! - Click Enter to try again"}
+        {isLoser && "Nice try - Click Enter to try again"}
+      </div>
+      <HangmanDrawing numberOfGuesses={incorrectLetters.length} />
+      <HangmanWord guessedLetters={guessedLetters} wordToGuess={wordToGuess} reveal={isLoser} />
+      <div
+        style={{ alignSelf: "stretch" }}>
+        <Keyboard
+          disabled={isWinner || isLoser}
+          activeLetters={guessedLetters.filter(letter => wordToGuess.includes(letter))}
+          inactiveLetters={incorrectLetters}
+          addGuessedLetter={addGuessedLetter}
+        />
+      </div>
     </div>
   )
 }
--- hangman/src/HangmanDrawing.tsx
+++ hangman/src/HangmanDrawing.tsx
@@ -7,73 +7,73 @@ const HEAD = (
         position: "absolute",
         top: "50px",
         right: "-30px"
-    }}></div>
+    }}key={0}></div>
 )
 const BODY = (
     <div style={{
         width: "10px",
         height: "80px",
-        background:"black",
+        background: "black",
         position: "absolute",
         top: "120px",
         right: "0px"
-    }}></div>
+    }}key={1}></div>
 )
 const LEFT_ARM = (
     <div style={{
         width: "10px",
         height: "40px",
-        background:"black",
+        background: "black",
         position: "absolute",
         transform: "rotate(45deg)",
         top: "120px",
         right: "15px"
-    }}></div>
+    }}key={2}></div>
 )
 const RIGHT_ARM = (
     <div style={{
         width: "10px",
         height: "40px",
-        background:"black",
+        background: "black",
         position: "absolute",
         transform: "rotate(135deg)",
         top: "120px",
         right: "-15px"
-    }}></div>
+    }}key={3}></div>
 )
 const LEFT_LEG = (
     <div style={{
         width: "10px",
         height: "40px",
-        background:"black",
+        background: "black",
         position: "absolute",
         transform: "rotate(45deg)",
         top: "190px",
         right: "15px"
-    }}></div>
+    }}key={4}></div>
 )
 const RIGHT_LEG = (
     <div style={{
         width: "10px",
         height: "40px",
-        background:"black",
+        background: "black",
         position: "absolute",
         transform: "rotate(135deg)",
         top: "190px",
         right: "-15px"
-    }}></div>
+    }} key={5}></div>
 )
-export function HangmanDrawing() {
-    return <div style={{position: "relative"}}>
-        {HEAD}
-        {BODY}
-        {LEFT_ARM}
-        {RIGHT_ARM}
-        {LEFT_LEG}
-        {RIGHT_LEG}
-        <div style={{height: "10px", width: "200px", background:"black", marginLeft: "120px"}}/>
-        <div style={{height: "50px", width: "10px", background:"black", marginLeft: "310px", position: "absolute", top: 0, right: 0}}/>
-        <div style={{height: "400px", width: "10px", background:"black", marginLeft: "120px"}}/>
-        <div style={{height: "10px", width: "250px", background:"black"}}/>
+const BODY_PARTS = [HEAD,BODY,RIGHT_ARM,LEFT_ARM,RIGHT_LEG,LEFT_LEG];
+type HangmanDrawingProps = {
+    numberOfGuesses: number
+}
+export function HangmanDrawing({ numberOfGuesses }: HangmanDrawingProps) {
+    const parts = numberOfGuesses < BODY_PARTS.length ?BODY_PARTS.slice(0,numberOfGuesses) : BODY_PARTS;
+    return <div style={{ position: "relative" }}>
+        {parts}
+        <div style={{ height: "10px", width: "200px", background: "black", marginLeft: "120px" }} />
+        <div style={{ height: "50px", width: "10px", background: "black", marginLeft: "310px", position: "absolute", top: 0, right: 0 }} />
+        <div style={{ height: "400px", width: "10px", background: "black", marginLeft: "120px" }} />
+        <div style={{ height: "10px", width: "250px", background: "black" }} />
     </div>
 }
